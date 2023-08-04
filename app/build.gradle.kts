7b60fc477d32badabd82fb05d282b9e021d9ef3c
Billie <Billie@supergene.co>
2023-08-04 16:08:48 +0900

    add kapt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
 }
 
 android {
@@ -64,6 +65,7 @@ dependencies {
     //Room
     implementation(libs.androidx.room.runtime)
     annotationProcessor(libs.androidx.room.compiler)
+    kapt(libs.androidx.room.compiler)
 
     // test
     testImplementation(libs.junit)

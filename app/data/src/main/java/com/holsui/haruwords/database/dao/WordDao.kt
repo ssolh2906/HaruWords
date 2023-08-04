8b545680b303854ba6e63ea367524f198c099f1f
Billie <Billie@supergene.co>
2023-08-04 18:12:30 +0900

    add Word Dao

---
+++ app/data/src/main/java/com/holsui/haruwords/database/dao/WordDao.kt
@@ -0,0 +1,20 @@
+package com.holsui.haruwords.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.holsui.haruwords.data.models.WordEntity
+
+@Dao
+interface WordDao {
+
+    @Query("SELECT * FROM words")
+    fun getAll(): List<WordEntity>
+
+    @Insert
+    fun insertWords(vararg words: WordEntity)
+
+    @Delete
+    fun deleteWords(vararg words: WordEntity)
+}

ea8dff3de5ca1553c4a413d73c1e238b6574b681
Billie <Billie@supergene.co>
2023-08-08 16:43:13 +0900

    add custom result class

--- app/domain/src/main/java/com/holsui/haruwords/domain/Result.kt
+++ app/domain/src/main/java/com/holsui/haruwords/domain/Result.kt
@@ -1,4 +1,7 @@
 package com.holsui.haruwords.domain
 
-class Result {
+sealed interface Result<out T> {
+    data class Success<T>(val data: T) : Result<T>
+    data class Failure(val error: Error) : Result<Nothing>
+    object Loading : Result<Nothing>
 }

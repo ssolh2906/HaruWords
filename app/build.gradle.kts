2f56f2cd1dbd2a0725a3d5e0983023d7898d4da4
Solhee Park <solheepark29@gmail.com>
2023-10-13 15:27:45 -0700

    update dependencies and add material3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,8 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha09")
+    implementation("androidx.compose.material3:material3-window-size-class:1.2.0-alpha09")
 
     // Room
     implementation(libs.androidx.room.runtime)

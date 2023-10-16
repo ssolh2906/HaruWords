c0b0b469a44f434643cc4d8d1b124fa25c9f1bdd
Solhee <solheepark29@gmail.com>
2023-10-16 11:17:47 -0700

    Add FAB UI

--- app/feature/src/main/java/com/holsui/haruwords/feature/wordslist/WordsListScreen.kt
+++ app/feature/src/main/java/com/holsui/haruwords/feature/wordslist/WordsListScreen.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -31,12 +33,34 @@ fun WordsListRoute(
 
 @Composable
 fun WordsListScreen(
-    wordList: List<Word>
+    wordList: List<Word>,
+    modifier: Modifier = Modifier
 ) {
-    LazyColumn(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+
+
+    Box(modifier = modifier.fillMaxSize()) {
+        WordList(wordList)
+
+        FloatingActionButton(
+            onClick = {},
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(24.dp)
+        ) {
+        }
+    }
+}
+
+@Composable
+private fun WordList(wordList: List<Word>) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = Modifier.padding(12.dp)
+    ) {
         items(wordList) { word ->
             Box(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
                     .height(100.dp)
                     .background(Color.White)
                     .padding(12.dp),
@@ -46,6 +70,8 @@ fun WordsListScreen(
                     Text(text = word.word)
                     Text(text = word.mean)
                 }
+
+
             }
         }
     }
